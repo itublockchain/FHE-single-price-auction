affd026aed3d7961ed86dfdcd39e6bb30087c90e
akifdundar <157687046+akifdundar@users.noreply.github.com>
2025-02-05 22:07:19 +0300

    Update IAuction.sol

--- contracts/interfaces/IAuction.sol
+++ contracts/interfaces/IAuction.sol
@@ -4,7 +4,7 @@ interface IAuction {
     event BidSubmitted(uint256 indexed bidId, address indexed bidder, uint256 timestamp);
 
     function submitBid(
-        bytes calldata encPrice,
+        einput encPrice,
         bytes calldata priceProof,
         uint256 amount
     ) external returns (uint256 bidId);
