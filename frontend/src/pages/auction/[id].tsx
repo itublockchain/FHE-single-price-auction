44da3e3fc194b58ab407d7f28e74050cc1a6fa25
Ömer Furkan Yürük <yuruko22@itu.edu.tr>
2025-02-05 22:41:25 +0300

    Base for Create Page

--- frontend/src/pages/auction/[id].tsx
+++ frontend/src/pages/auction/[id].tsx
@@ -1,10 +1,10 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
-export default function Home() {
-
-    const router = useRouter();
-    return (<div className="w-full h-screen flex justify-center items-center bg-gray-500">
+export default function Auction() {
+  const router = useRouter();
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-gray-500">
       <p>Auction: {router.query.id ? router.query.id : "None"}</p>
-    </div>);
-  }
-  
+    </div>
+  );
+}
--- frontend/src/pages/create/index.tsx
+++ frontend/src/pages/create/index.tsx
@@ -1,5 +1,156 @@
-export default function Home() {
-  return (<div className="w-full h-screen flex justify-center items-center bg-gray-500">
-    <p>Create auction page</p>
-  </div>);
+"use client";
+
+import * as React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns"
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(5, {
+      message: "Title must be at least 5 characters.",
+    })
+    .max(20, {
+      message: "Title can be at most 20 characters.",
+    }),
+  amount: z.string(),
+  description: z.string(),
+  date: z.date(),
+});
+
+export default function Create() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      amount: "",
+      description: "",
+      date: new Date(),
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <Form {...form}>
+        <h1 className="text-7xl">Create Auction</h1>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 bg-white p-6 rounded-lg shadow-md w-2/3"
+        >
+          <div className="grid grid-cols-3 gap-4">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name of your project" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter amount" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Date</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full justify-center text-left font-normal",
+                            !date && "text-muted-foreground"
+                          )}
+                        >
+                          {date ? (
+                            format(date, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={date}
+                          onSelect={setDate}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <textarea
+                    className="w-full border rounded-md p-2"
+                    placeholder="Write a description"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end space-x-4">
+            <Button type="button" variant="outline">
+              Cancel
+            </Button>
+            <Button type="submit">Create Auction</Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 }
