84ff02a08915477afec2fce68cf7574ed2ab9cd4
Ömer Furkan Yürük <yuruko22@itu.edu.tr>
2025-02-07 16:40:19 +0300

    Design of auction page and dynamic routing for autctions

---
+++ frontend/app/[auction]/page.tsx
@@ -0,0 +1,98 @@
+import Image from "next/image";
+import hourglass from "../../public/HourglassMedium.svg";
+import rocket from "../../public/RocketLaunch.svg";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+
+const auctionDetails = {
+  title: "Car Auction",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam. Donec nec nunc nec nunc tincidunt aliquam.",
+  amount: "10000",
+  date: new Date(),
+};
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ auction: string }>;
+}) {
+  const slug = (await params).auction;
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <div className="w-3/4 h-3/4 bg-background dark:text-foreground p-8 rounded-lg flex justify-center items-left flex-col gap-4">
+        <div>
+          <h1 className="text-7xl font-bold mb-2">
+            {auctionDetails.title + " " + slug}
+            <span className="text-3xl font-normal ml-2">
+              x{auctionDetails.amount}
+            </span>
+          </h1>
+          <span className="text-foreground/50">
+            by<span className="ml-1 text-foreground">0xabc783...d84</span>
+          </span>
+        </div>
+        <p className="text-lg my-12">{auctionDetails.description}</p>
+        <div className="flex justify-between items-center w-full">
+          <div className="flex items-left gap-8">
+            <p className="text-lg flex gap-1 text-foreground/60">
+              <Image alt="rocket" src={rocket} />
+              {auctionDetails.date.toLocaleDateString()}
+            </p>
+            <p className="text-lg flex gap-1 text-foreground/60">
+              <Image alt="hourglass" src={hourglass} />
+              {auctionDetails.date.toLocaleDateString()}
+            </p>
+            <p className="text-lg">
+              {"4h 20m"}
+              <span className="text-foreground/60 ml-2">Left</span>
+            </p>
+          </div>
+          <div>
+            <Drawer>
+              <DrawerTrigger asChild>
+                <Button>Place Bid</Button>
+              </DrawerTrigger>
+              <DrawerContent className="flex flex-col justify-center items-center">
+                <DrawerHeader className="w-1/4">
+                  <DrawerTitle>Place a Bid</DrawerTitle>
+                  <DrawerDescription>
+                    Enter bid price and quantity. The cost will be calculated.
+                  </DrawerDescription>
+                  <div className="flex justify-between items-center gap-4">
+                    <label className="text-sm">Amount</label>
+                  <Input className="w-[250px]" placeholder="Enter amount" />
+                  </div>
+                  <div className="flex justify-between items-center gap-4">
+                    <label className="text-sm">Price</label>
+                  <Input className="w-[250px]" placeholder="Enter price" />
+                  </div>
+                  <div className="h-[1px] w-full bg-white/20 my-4"></div>
+                  <p>Total Cost: 1.0 ETH</p>
+                </DrawerHeader>
+                <DrawerFooter className="w-1/4">
+                  <Button className="w-full">Confirm Bid</Button>
+                  <DrawerClose className="w-full">
+                    <Button variant="outline" className="w-full">
+                      Cancel
+                    </Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,4 +1,7 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+"use client"
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
     return (<div className="w-full border-b-[1px] absolute px-[150px] h-[80px] bg-background text-foreground flex items-center justify-around">
@@ -9,6 +12,93 @@ export default function Navbar() {
             <div>GitHub</div>
             <div>Medium</div>
         </div>
-        <div><ConnectButton /></div>
+        <div><ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== 'loading';
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus ||
+            authenticationStatus === 'authenticated');
+        return (
+          <div
+            {...(!ready && {
+              'aria-hidden': true,
+              'style': {
+                opacity: 0,
+                pointerEvents: 'none',
+                userSelect: 'none',
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button onClick={openConnectModal} type="button">
+                    Connect Wallet
+                  </Button>
+                );
+              }
+              if (chain.unsupported) {
+                return (
+                  <Button onClick={openChainModal} type="button">
+                    Wrong network
+                  </Button>
+                );
+              }
+              return (
+                <div style={{ display: 'flex', gap: 4 }}>
+                  <Button
+                    onClick={openChainModal}
+                    style={{ display: 'flex', alignItems: 'center' }}
+                    type="button"
+                  >
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          width: 24,
+                          height: 24,
+                          borderRadius: 999,
+                          overflow: 'hidden',
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <img
+                            alt={chain.name ?? 'Chain icon'}
+                            src={chain.iconUrl}
+                            style={{ width: 24, height: 24 }}
+                          />
+                        )}
+                      </div>
+                    )}
+                  </Button>
+                  <Button onClick={openAccountModal} type="button">
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ''}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom></div>
     </div>);
 }
